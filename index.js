df8ae7019a21b6f1091badadc4c7ef46af097268
EDILSON APARECIDO DE PAULA SOUZA <117594708+EdilsonAparecido@users.noreply.github.com>
2022-11-30 06:41:03 -0300

    adicionando arquivo da aula1

---
+++ index.js
@@ -0,0 +1,12 @@
+let nome = "Edilson" //string
+let idade = 90 //number
+let logado = true //boolean
+let lista = ["ovo", "massa", "corante"] //array
+let nomeCompleto = {nome: "Edilson", idade: 90} //object
+
+// o comando abaixo exibe os dados no Console
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof logado)
+console.log(typeof lista)
+console.log(typeof nomeCompleto)
