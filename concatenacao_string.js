c5f8f475d969d201cbfa62fe9c1b6d3c56b6b163
EDILSON APARECIDO DE PAULA SOUZA <117594708+EdilsonAparecido@users.noreply.github.com>
2022-11-30 07:09:31 -0300

    adicionando concatenação de string

---
+++ concatenacao_string.js
@@ -0,0 +1,15 @@
+let nome = "Edilson";
+let sobrenome = "Aparecido";
+let nomeCompleto = nome + ' ' + sobrenome + ' Ok';
+console.log(nomeCompleto);
+
+let moeda = "R";
+let simbolo = "$";
+let valor = 19.90
+
+let moedaSimbolo = moeda + simbolo + valor;
+console.log(moedaSimbolo);
+
+let novoNomeCompleto = `${nome} ${sobrenome} Ok`;
+
+console.log(novoNomeCompleto);
