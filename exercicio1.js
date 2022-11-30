cee917a3c000af661a6841be7099215ac4c4d9ed
EDILSON APARECIDO DE PAULA SOUZA <117594708+EdilsonAparecido@users.noreply.github.com>
2022-11-30 06:49:39 -0300

    adicionando exercicio1

---
+++ exercicio1.js
@@ -0,0 +1,13 @@
+// 1. Crie uma variável para armazenar o nome
+// // do carro e ponha o valor Ferrari. E exiba-a.
+
+// 2. Crie uma variável para armazenar o preço
+// do bolo, que custa R$19,99. E exiba-a.
+
+let preco = 19.99;
+let carro = "Ferrari";
+let cidade = "São Paulo";
+
+console.log(carro)
+console.log(preco)
+console.log(cidade)
