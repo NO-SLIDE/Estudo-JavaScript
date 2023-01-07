8bfaf76f49b7769ad21a11e159ecc7c7c4279f3a
Edilson Aparecido de Paula Souza <edilson.action2@gmail.com>
2023-01-06 23:10:56 -0300

    adicionando novas alterações

--- academia.js
+++ academia.js
@@ -125,20 +125,20 @@ registroAlunosAcademia[0].treinos = {
     },
     {
       nome: listaExerciciosPorTipo.Peito.Exercicios[1],
-      repeticoes: 10,
-      series: 3,
+      repeticoes: 8,
+      series: 4,
       pausa: 60,
       tempoCadaMovimento: 4,
     },
     {
       nome: listaExerciciosPorTipo.Peito.Exercicios[2],
-      repeticoes: 10,
-      series: 3,
+      repeticoes: 8,
+      series: 4,
       pausa: 60,
       tempoCadaMovimento: 4,
     },
     {
-      nome: listaExerciciosPorTipo.Peito.Exercicios[3],
+      nome: listaExerciciosPorTipo.Peito.Exercicios[4],
       repeticoes: 10,
       series: 3,
       pausa: 60,
@@ -146,31 +146,31 @@ registroAlunosAcademia[0].treinos = {
     },
     {
       nome: listaExerciciosPorTipo.Biceps.Exercicios[0],
-      repeticoes: 10,
-      series: 3,
+      repeticoes: 8,
+      series: 4,
       pausa: 60,
-      tempoCadaMovimento: 4,
+      tempoCadaMovimento: 3,
     },
     {
       nome: listaExerciciosPorTipo.Biceps.Exercicios[1],
       repeticoes: 10,
       series: 3,
       pausa: 60,
-      tempoCadaMovimento: 4,
+      tempoCadaMovimento: 3,
     },
     {
       nome: listaExerciciosPorTipo.Biceps.Exercicios[2],
-      repeticoes: 10,
-      series: 3,
+      repeticoes: 8,
+      series: 4,
       pausa: 60,
-      tempoCadaMovimento: 4,
+      tempoCadaMovimento: 3,
     },
     {
       nome: listaExerciciosPorTipo.Biceps.Exercicios[3],
       repeticoes: 10,
       series: 3,
       pausa: 60,
-      tempoCadaMovimento: 4,
+      tempoCadaMovimento: 3,
     },
     {
       nome: listaExerciciosPorTipo.Abdominal.Exercicios[1],
@@ -228,34 +228,34 @@ registroAlunosAcademia[0].treinos = {
       repeticoes: 8,
       series: 4,
       pausa: 60,
-      tempoCadaMovimento: 4,
+      tempoCadaMovimento: 3,
     },
     {
       nome: listaExerciciosPorTipo.Triceps.Exercicios[1],
       repeticoes: 8,
       series: 4,
       pausa: 60,
-      tempoCadaMovimento: 4,
+      tempoCadaMovimento: 3,
     },
     {
       nome: listaExerciciosPorTipo.Triceps.Exercicios[2],
       repeticoes: 8,
       series: 4,
       pausa: 60,
-      tempoCadaMovimento: 4,
+      tempoCadaMovimento: 3,
     },
     {
       nome: listaExerciciosPorTipo.Panturrilha.Exercicios[0],
       repeticoes: 15,
       series: 3,
-      pausa: 60,
+      pausa: 30,
       tempoCadaMovimento: 4,
     },
     {
       nome: listaExerciciosPorTipo.Panturrilha.Exercicios[1],
       repeticoes: 15,
       series: 3,
-      pausa: 60,
+      pausa: 30,
       tempoCadaMovimento: 4,
     },
     {
@@ -267,24 +267,24 @@ registroAlunosAcademia[0].treinos = {
     },
     {
       nome: listaExerciciosPorTipo.Abdominal.Exercicios[9],
-      repeticoes: 20,
+      repeticoes: 12,
       series: 3,
-      pausa: 60,
+      pausa: 45,
       tempoCadaMovimento: 4,
     },
   ],
   Treino_C: [
     {
       nome: listaExerciciosPorTipo.Ombro.Exercicios[0],
-      repeticoes: 10,
-      series: 3,
+      repeticoes: 8,
+      series: 4,
       pausa: 60,
       tempoCadaMovimento: 4,
     },
     {
       nome: listaExerciciosPorTipo.Ombro.Exercicios[1],
-      repeticoes: 10,
-      series: 3,
+      repeticoes: 8,
+      series: 4,
       pausa: 60,
       tempoCadaMovimento: 4,
     },
@@ -318,8 +318,8 @@ registroAlunosAcademia[0].treinos = {
     },
     {
       nome: listaExerciciosPorTipo.Coxa.Exercicios[2],
-      repeticoes: 10,
-      series: 3,
+      repeticoes: 8,
+      series: 4,
       pausa: 60,
       tempoCadaMovimento: 4,
     },
@@ -356,58 +356,74 @@ registroAlunosAcademia[0].treinos = {
 
 
 console.table(registroAlunosAcademia);
-console.log(`Treino do ${registroAlunosAcademia[0].nome}`);
+console.log(`Treino do ${registroAlunosAcademia[0].nome}:`);
+console.log(registroAlunosAcademia[0].treinos);
+
+console.log(`Treino do ${registroAlunosAcademia[1].nome}:`);
 console.log(registroAlunosAcademia[0].treinos);
 
 
 let treinoA = registroAlunosAcademia[0].treinos.Treino_A;
 
-let r1 = `${treinoA[0].repeticoes + treinoA[1].repeticoes + treinoA[2].repeticoes + treinoA[3].repeticoes + treinoA[4].repeticoes + treinoA[5].repeticoes + treinoA[6].repeticoes + treinoA[7].repeticoes + treinoA[8].repeticoes + treinoA[9].repeticoes + treinoA[10].repeticoes}`;
-console.log(r1);
+let r1 = treinoA[0].repeticoes + treinoA[1].repeticoes + treinoA[2].repeticoes + treinoA[3].repeticoes + treinoA[4].repeticoes + treinoA[5].repeticoes + treinoA[6].repeticoes + treinoA[7].repeticoes + treinoA[8].repeticoes + treinoA[9].repeticoes + treinoA[10].repeticoes;
+// console.log(r1);
 
-let s1 = `${treinoA[0].series + treinoA[1].series + treinoA[2].series + treinoA[3].series + treinoA[4].series + treinoA[5].series + treinoA[6].series + treinoA[7].series + treinoA[8].series + treinoA[9].series + treinoA[10].series}`;
-console.log(s1);
+let s1 = treinoA[0].series + treinoA[1].series + treinoA[2].series + treinoA[3].series + treinoA[4].series + treinoA[5].series + treinoA[6].series + treinoA[7].series + treinoA[8].series + treinoA[9].series + treinoA[10].series;
+// console.log(s1);
 
-let p1 = `${treinoA[0].pausa + treinoA[1].pausa + treinoA[2].pausa + treinoA[3].pausa + treinoA[4].pausa + treinoA[5].pausa + treinoA[6].pausa + treinoA[7].pausa + treinoA[8].pausa + treinoA[9].pausa + treinoA[10].pausa}`;
-console.log(p1);
+let p1 = treinoA[0].pausa + treinoA[1].pausa + treinoA[2].pausa + treinoA[3].pausa + treinoA[4].pausa + treinoA[5].pausa + treinoA[6].pausa + treinoA[7].pausa + treinoA[8].pausa + treinoA[9].pausa + treinoA[10].pausa;
+// console.log(p1);
 
-let m1 = `${treinoA[0].tempoCadaMovimento + treinoA[1].tempoCadaMovimento + treinoA[2].tempoCadaMovimento + treinoA[3].tempoCadaMovimento + treinoA[4].tempoCadaMovimento + treinoA[5].tempoCadaMovimento + treinoA[6].tempoCadaMovimento + treinoA[7].tempoCadaMovimento + treinoA[8].tempoCadaMovimento + treinoA[9].tempoCadaMovimento + treinoA[10].tempoCadaMovimento}`;
-console.log(m1);
+let m1 = treinoA[0].tempoCadaMovimento + treinoA[1].tempoCadaMovimento + treinoA[2].tempoCadaMovimento + treinoA[3].tempoCadaMovimento + treinoA[4].tempoCadaMovimento + treinoA[5].tempoCadaMovimento + treinoA[6].tempoCadaMovimento + treinoA[7].tempoCadaMovimento + treinoA[8].tempoCadaMovimento + treinoA[9].tempoCadaMovimento + treinoA[10].tempoCadaMovimento;
+// console.log(m1);
+
+let calculoTempoTreinoA = (((m1 * r1) * s1)/ 60) + ((p1 * s1) / 60);
+// console.log(calculoTempoTreinoA);
 
 
 
 let treinoB = registroAlunosAcademia[0].treinos.Treino_B;
 
 let r2 = `${treinoB[0].repeticoes + treinoB[1].repeticoes + treinoB[2].repeticoes + treinoB[3].repeticoes + treinoB[4].repeticoes + treinoB[5].repeticoes + treinoB[6].repeticoes + treinoB[7].repeticoes + treinoB[8].repeticoes + treinoB[9].repeticoes + treinoB[10].repeticoes}`;
-console.log(r2);
+// console.log(r2);
 
 let s2 = `${treinoB[0].series + treinoB[1].series + treinoB[2].series + treinoB[3].series + treinoB[4].series + treinoB[5].series + treinoB[6].series + treinoB[7].series + treinoB[8].series + treinoB[9].series + treinoB[10].series}`;
-console.log(s2);
+// console.log(s2);
 
 let p2 = `${treinoB[0].pausa + treinoB[1].pausa + treinoB[2].pausa + treinoB[3].pausa + treinoB[4].pausa + treinoB[5].pausa + treinoB[6].pausa + treinoB[7].pausa + treinoB[8].pausa + treinoB[9].pausa + treinoB[10].pausa}`;
-console.log(p2);
+// console.log(p2);
 
 let m2 = `${treinoB[0].tempoCadaMovimento + treinoB[1].tempoCadaMovimento + treinoB[2].tempoCadaMovimento + treinoB[3].tempoCadaMovimento + treinoB[4].tempoCadaMovimento + treinoB[5].tempoCadaMovimento + treinoB[6].tempoCadaMovimento + treinoB[7].tempoCadaMovimento + treinoB[8].tempoCadaMovimento + treinoB[9].tempoCadaMovimento + treinoB[10].tempoCadaMovimento}`;
-console.log(m2);
+// console.log(m2);
+
+let calculoTempoTreinoB = (((m2 * r2) * s2)/ 60) + ((p2 * s2) / 60);
+// console.log(calculoTempoTreinoB);
 
 
 
 let treinoC = registroAlunosAcademia[0].treinos.Treino_C;
 
 let r3 = `${treinoC[0].repeticoes + treinoC[1].repeticoes + treinoC[2].repeticoes + treinoC[3].repeticoes + treinoC[4].repeticoes + treinoC[5].repeticoes + treinoC[6].repeticoes + treinoC[7].repeticoes + treinoC[8].repeticoes + treinoC[9].repeticoes + treinoC[10].repeticoes}`;
-console.log(r3);
+// console.log(r3);
 
 let s3 = `${treinoC[0].series + treinoC[1].series + treinoC[2].series + treinoC[3].series + treinoC[4].series + treinoC[5].series + treinoC[6].series + treinoC[7].series + treinoC[8].series + treinoC[9].series + treinoC[10].series}`;
-console.log(s3);
+// console.log(s3);
 
 let p3 = `${treinoC[0].pausa + treinoC[1].pausa + treinoC[2].pausa + treinoC[3].pausa + treinoC[4].pausa + treinoC[5].pausa + treinoC[6].pausa + treinoC[7].pausa + treinoC[8].pausa + treinoC[9].pausa + treinoC[10].pausa}`;
-console.log(p3);
+// console.log(p3);
 
 let m3 = `${treinoC[0].tempoCadaMovimento + treinoC[1].tempoCadaMovimento + treinoC[2].tempoCadaMovimento + treinoC[3].tempoCadaMovimento + treinoC[4].tempoCadaMovimento + treinoC[5].tempoCadaMovimento + treinoC[6].tempoCadaMovimento + treinoC[7].tempoCadaMovimento + treinoC[8].tempoCadaMovimento + treinoC[9].tempoCadaMovimento + treinoC[10].tempoCadaMovimento}`;
-console.log(m3);
+// console.log(m3);
+
+let calculoTempoTreinoC = (((m3 * r3) * s3)/ 60) + ((p3 * s3) / 60);
+// console.log(calculoTempoTreinoC);
 
 
+console.log(`O treino A do aluno Edilson dura em média ${calculoTempoTreinoA / 60} minutos`);
+console.log(`O treino B do aluno Edilson dura em média ${calculoTempoTreinoA / 60} minutos`);
+console.log(`O treino C do aluno Edilson dura em média ${calculoTempoTreinoA / 60} minutos`);
 
-
-
+console.log(`O treino A do aluno João dura em média ${calculoTempoTreinoA / 60} minutos`);
+console.log(`O treino B do aluno João dura em média ${calculoTempoTreinoA / 60} minutos`);
+console.log(`O treino C do aluno João dura em média ${calculoTempoTreinoA / 60} minutos`);
 
