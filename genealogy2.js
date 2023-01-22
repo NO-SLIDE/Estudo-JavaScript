82f2df2ab7f45399b9d06201f39655af0ee4761f
Edilson Aparecido de Paula Souza <edilson.action2@gmail.com>
2023-01-22 12:24:31 -0300

    adicionando código da árvore genealógica finalizada

--- genealogy2.js
+++ genealogy2.js
@@ -94,34 +94,413 @@ let pessoas = [
     idade: 9,
     sexo: "masculino",
     },
+    {
+    nome: "Vivian",
+    idade: 39,
+    sexo: "feminino",
+    },
+    {
+    nome: "Gabriel",
+    idade: 15,
+    sexo: "masculino",
+    },
+    {
+    nome: "Floro",
+    idade: 70,
+    sexo: "masculino",
+    },
+    {
+    nome: "Flavio",
+    idade: 64,
+    sexo: "masculino",     
+    },
+    {
+    nome: "Flauviano",
+    idade: 65,
+    sexo: "masculino",
+    },
+    {
+    nome: "Fernando",
+    idade: 69,
+    sexo: "masculino",
+    },
+    {
+    nome: "Pereira",
+    idade: 64,
+    sexo: "masculino",
+    },
+    {
+    nome: "Bosco",
+    idade: 63,
+    sexo: "masculino",
+    },
+    {
+    nome: "Zelia",
+    idade: 71,
+    sexo: "feminino",
+    },
+    {
+    nome: "Leda",
+    idade: 69,
+    sexo: "feminino",
+    },
+    {
+    nome: "Fátima",
+    idade: 68,
+    sexo: "feminino",
+    },
+    {
+    nome: "Verônica",
+    idade: 67,
+    sexo: "feminino",
+    },
+    {
+    nome: "Mônica",
+    idade: 66,
+    sexo: "feminino",
+    },
+    {
+    nome: "Marta",
+    idade: 64,
+    sexo: "feminino",
+    },
+    {
+    nome: "Lourival",
+    idade: 56,
+    sexo: "masculino",
+    },
+    {
+    nome: "Sebastião",
+    idade: 67,
+    sexo: "masculino",
+    },   
+    {
+    nome: "Antenor",
+    idade: 68,
+    sexo: "masculino",
+    },
+    {
+    nome: "Gonçalo",
+    idade: 67,
+    sexo: "masculino",
+    },
+    {
+    nome: "Darque",
+    idade: 65,
+    sexo: "masculino",
+    },
+    {
+    nome: "Aninha",
+    idade: 82,
+    sexo: "feminino",
+    },
+    {
+    nome: "Candinha",
+    idade: 72,
+    sexo: "feminino",
+    },
+    {
+    nome: "Clara",
+    idade: 75,
+    sexo: "feminino",
+    },
+    {
+    nome: "Maria",
+    idade: 71,
+    sexo: "feminino",
+    },
+    {
+    nome: "Benedita",
+    idade: 68,
+    sexo: "feminino",
+    },
+    {
+    nome: "Paulinha",
+    idade: 42,
+    sexo: "feminino",
+    },
+    {
+    nome: "Pedro",
+    idade: 45,
+    sexo: "masculino",
+    },
+    {
+    nome: "Flavio",
+    idade: 47,
+    sexo: "masculino",
+    },
+    {
+    nome: "Tania",
+    idade: 45,
+    sexo: "feminino",
+    },
+    {
+    nome: "João Paulo",
+    idade: 38,
+    sexo: "masculino",
+    },
+    {
+    nome: "Fabinho",
+    idade: 33,
+    sexo: "masculino",
+    },
+    {
+    nome: "Luciana",
+    idade: 30,
+    sexo: "feminino",
+    },
+    {
+    nome: "Dejanira",
+    idade: 38,
+    sexo: "feminino",
+    },
+    {
+    nome: "Daniel",
+    idade: 48,
+    sexo: "masculino",
+    },
+    {
+    nome: "Daniela",
+    idade: 35,
+    sexo: "feminino",
+    },
+    {
+    nome: "Cleide",
+    idade: 39,
+    sexo: "feminino",
+    },
+    {
+    nome: "Aparecida",
+    idade: 37,
+    sexo: "feminino",
+    },
+    {
+    nome: "Viviane",
+    idade: 35,
+    sexo: "feminino",
+    },
+    {
+    nome: "Lara",
+    idade: 25,
+    sexo: "feminino",
+    },
+    {
+    nome: "Larissa",
+    idade: 22,
+    sexo: "feminino",
+    },
+    {
+    nome: "Lucas",
+    idade: 18,
+    sexo: "masculino",
+    },
+    {
+    nome: "Henrique",
+    idade: 45,
+    sexo: "masculino",
+    },
+    {
+    nome: "Gustavo",
+    idade: 42,
+    sexo: "masculino",
+    },
+    {
+    nome: "Edna",
+    idade: 45,
+    sexo: "feminino",
+    },
+    {
+    nome: "Renato",
+    idade: 43,
+    sexo: "masculino",
+    },
+    {
+    nome: "Renata",
+    idade: 39,
+    sexo: "feminino",
+    },
+    {
+    nome: "Ednaldo",
+    idade: 37,
+    sexo: "masculino",
+    },
+    {
+    nome: "Paola",
+    idade: 36,
+    sexo: "feminino",
+    },
+    {
+    nome: "Pamela",
+    idade: 36,
+    sexo: "feminino",
+    },
+    {
+    nome: "Pietro",
+    idade: 34,
+    sexo: "masculino",
+    },
+    {
+    nome: "Vanessa",
+    idade: 36,
+    sexo: "feminino",
+    },
+    {
+    nome: "Rodrigo",
+    idade: 42,
+    sexo: "masculino",
+    },
+    {
+    nome: "Carla",
+    idade: 38,
+    sexo: "feminino",
+    },
+    {
+    nome: "Willian",
+    idade: 36,
+    sexo: "masculino",
+    },
+    {
+    nome: "Sandro",
+    idade: 36,
+    sexo: "masculino",
+    },
+    {
+    nome: "Gil",
+    idade: 40,
+    sexo: "masculino",
+    },
+    {
+    nome: "Darquinho",
+    idade: 36,
+    sexo: "masculino",
+    },
+    {
+    nome: "Leandro",
+    idade: 33,
+    sexo: "masculino",
+    },
+    {
+    nome: "Alexandrino",
+    idade: 50,
+    sexo: "masculino",
+    },
+    {
+    nome: "Rogério",
+    idade: 44,
+    sexo: "masculino",
+    },
+    {
+    nome: "Elza",
+    idade: 40,
+    sexo: "feminino",
+    },
+    {
+    nome: "Ivanilton",
+    idade: 44,
+    sexo: "masculino",
+    },
+    {
+    nome: "Demerson",
+    idade: 36,
+    sexo: "masculino",
+    },
+    {
+    nome: "Sonia",
+    idade: 40,
+    sexo: "feminino",
+    },
+    {
+    nome: "Carlos",
+    idade: 44,
+    sexo: "masculino",
+    },
+    {
+    nome: "Adriano",
+    idade: 38,
+    sexo: "masculino",
+    },
+    {
+    nome: "Moacir",
+    idade: 30,
+    sexo: "masculino",
+    },
+    {
+    nome: "Sebastiana",
+    idade: 44,
+    sexo: "feminino",
+    },
+    {
+    nome: "Marcelo",
+    idade: 42,
+    sexo: "masculino",
+    },
+    {
+    nome: "Gustavo",
+    idade: 44,
+    sexo: "masculino",
+    },
+    {
+    nome: "Luana",
+    idade: 36,
+    sexo: "feminino",
+    },
+    {
+    nome: "Leiliane",
+    idade: 42,
+    sexo: "feminino",
+    },
 ];
 
-pessoas[2].pais = [pessoas[0], pessoas[3], pessoas[4]];
-pessoas[1].pais = [pessoas[0], pessoas[3], pessoas[4]];
+
+pessoas[0].pais = [pessoas[1], pessoas[2]];
 pessoas[0].irmaos = [pessoas[3], pessoas[4]];
-pessoas[5].filhos = [pessoas[0]];
-pessoas[6].filhos = [pessoas[0]];
-pessoas[7].filhos = [pessoas[0]];
-pessoas[8].filhos = [pessoas[0]];
-pessoas[9].conjugue = [pessoas[0]];
-pessoas[10].avos_maternos = [pessoas[0], pessoas[3], pessoas[4]];
-pessoas[11].avos_maternos = [pessoas[0], pessoas[3], pessoas[4]];
-pessoas[12].avos_paternos = [pessoas[0], pessoas[3], pessoas[4]];
-pessoas[13].avos_paternos = [pessoas[0], pessoas[3], pessoas[4]];
-pessoas[14].sobrinhos = [pessoas[0]];
-pessoas[15].sobrinhos = [pessoas[0]];
-pessoas[16].sobrinhos = [pessoas[0]];
-pessoas[17].sobrinhos = [pessoas[0]];
-pessoas[18].sobrinhos = [pessoas[0]];
+pessoas[0].filhos = [pessoas[5], pessoas[6], pessoas[7], pessoas[8]];
+pessoas[0].enteado = [pessoas[20]];
+pessoas[0].conjugue = [pessoas[9]];
+pessoas[0].exConjugue = [pessoas[19]];
+pessoas[0].exConjugue[0].filhos = [pessoas[5], pessoas[6], pessoas[7]];
+pessoas[0].avos_maternos = [pessoas[10], pessoas[11]];
+pessoas[0].avos_maternos[0].filhos = [pessoas[21], pessoas[22], pessoas[23], pessoas[24], pessoas[25], pessoas[26], pessoas[27], pessoas[28], pessoas[29], pessoas[30], pessoas[31], pessoas[32]];
+pessoas[0].avos_maternos[0].filhos[0].filhos = [pessoas[43], pessoas[44]]; 
+pessoas[0].avos_maternos[0].filhos[3].filhos = [pessoas[45], pessoas[46], pessoas[47], pessoas[48], pessoas[49]];
+pessoas[0].avos_maternos[0].filhos[4].filhos = [pessoas[50], pessoas[51], pessoas[52]];
+pessoas[0].avos_maternos[0].filhos[5].filhos = [pessoas[53], pessoas[54], pessoas[55], pessoas[56], pessoas[57], pessoas[58]];
+pessoas[0].avos_maternos[0].filhos[6].filhos = [pessoas[59], pessoas[60]];
+pessoas[0].avos_maternos[0].filhos[7].filhos = [pessoas[0], pessoas[3], pessoas[4]];
+pessoas[0].avos_maternos[0].filhos[8].filhos = [pessoas[61], pessoas[62], pessoas[63], pessoas[64]];
+pessoas[0].avos_maternos[0].filhos[9].filhos = [pessoas[65], pessoas[66], pessoas[67]];
+pessoas[0].avos_maternos[0].filhos[10].filhos = [pessoas[68]];
+pessoas[0].avos_maternos[0].filhos[11].filhos = [pessoas[69], pessoas[70], pessoas[71]];
+pessoas[0].avos_paternos = [pessoas[12], pessoas[13]];
+pessoas[0].avos_paternos[0].filhos = [pessoas[33], pessoas[34], pessoas[35], pessoas[36], pessoas[37], pessoas[38], pessoas[39], pessoas[40], pessoas[41], pessoas[42]];
+pessoas[0].avos_paternos[0].filhos[0].filhos = [pessoas[72], pessoas[73]];
+pessoas[0].avos_paternos[0].filhos[2].filhos = [pessoas[0], pessoas[3], pessoas[4]];
+pessoas[0].avos_paternos[0].filhos[3].filhos = [pessoas[89]];
+pessoas[0].avos_paternos[0].filhos[4].filhos = [pessoas[74], pessoas[75]];
+pessoas[0].avos_paternos[0].filhos[5].filhos = [pessoas[76], pessoas[77], pessoas[78]];
+pessoas[0].avos_paternos[0].filhos[6].filhos = [pessoas[79], pessoas[80]];
+pessoas[0].avos_paternos[0].filhos[7].filhos = [pessoas[81], pessoas[82], pessoas[83]];
+pessoas[0].avos_paternos[0].filhos[8].filhos = [pessoas[84], pessoas[85], pessoas[86]];
+pessoas[0].avos_paternos[0].filhos[9].filhos = [pessoas[87], pessoas[88]];
+pessoas[0].primos = [pessoas[43], pessoas[44], pessoas[45], pessoas[46], pessoas[47], pessoas[48], pessoas[49], pessoas[50], pessoas[51], pessoas[52], pessoas[53], pessoas[54],pessoas[55], pessoas[56], pessoas[57], pessoas[58], pessoas[59], pessoas[60], pessoas[61], pessoas[62], pessoas[63], pessoas[64], pessoas[65], pessoas[66], pessoas[67], pessoas[68], pessoas[69], pessoas[70], pessoas[71], pessoas[72], pessoas[73], pessoas[74], pessoas[75], pessoas[76], pessoas[77], pessoas[78], pessoas[79], pessoas[80], pessoas[81], pessoas[82], pessoas[83], pessoas[84], pessoas[85], pessoas[86], pessoas[87], pessoas[88], pessoas[89]];
+pessoas[0].irmaos[0].filhos = [pessoas[14], pessoas[15], pessoas[16]];
+pessoas[0].irmaos[1].filhos = [pessoas[17], pessoas[18]];
+pessoas[0].irmaos[0].sobrinhos = [pessoas[0].filhos, pessoas[4].filhos];
+pessoas[0].irmaos[1].sobrinhos = [pessoas[0].filhos, pessoas[3].filhos];
+pessoas[0].conjugue[0].filhos = [pessoas[20], pessoas[8]];
+
 
 let arvoreEdilson = {
     ...pessoas[0],
     pais: [pessoas[1], pessoas[2]],
     irmaos: [pessoas[3], pessoas[4]],
     filhos: [pessoas[5], pessoas[6], pessoas[7], pessoas[8]],
+    enteado: [pessoas[20]],
     conjugue: [pessoas[9]],
+    exConjugue: [pessoas[19]],
     avos_maternos: [pessoas[10], pessoas[11]],
     avos_paternos: [pessoas[12], pessoas[13]],
+    primos: [pessoas[43], pessoas[44], pessoas[45], pessoas[46], pessoas[47], pessoas[48], pessoas[49], pessoas[50], pessoas[51], pessoas[52], pessoas[53], pessoas[54],pessoas[55], pessoas[56], pessoas[57], pessoas[58], pessoas[59], pessoas[60], pessoas[61], pessoas[62], pessoas[63], pessoas[64], pessoas[65], pessoas[66], pessoas[67], pessoas[68], pessoas[69], pessoas[70], pessoas[71], pessoas[72], pessoas[73], pessoas[74], pessoas[75], pessoas[76], pessoas[77], pessoas[78], pessoas[79], pessoas[80], pessoas[81], pessoas[82], pessoas[83], pessoas[84], pessoas[85], pessoas[86], pessoas[87], pessoas[88], pessoas[89]],
     sobrinhos: [pessoas[14], pessoas[15], pessoas[16], pessoas[17], pessoas[18]],
 };
 
