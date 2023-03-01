10e29c4c3dead3a3dfdd37e1347439c9833870d8
NO-SLIDE <edilson.action2@gmail.com>
2023-03-01 17:28:43 -0300

    primeiro commit

---
+++ exercícios-function.js
@@ -0,0 +1,109 @@
+//---- CARDÁPIO ----
+
+let lanches = [
+  {
+  nome: "Cheeseburguer",
+  ingredientes: ["pão de hambúrguer", "bacon", "cheddar", "cebola", "picles"],
+  valor: 15.90
+  },
+  {
+  nome: "X-Bacon",
+  ingredientes: ["pão de hambúrguer", "bacon fatiado", "cheddar", "maionese", ],
+  valor: 13.90
+  },
+  {
+  nome: "X-Burguer",
+  ingredientes: ["pão de hambúrguer", "carne bovina", "cheddar", "picles", "maionese"],
+  valor: 16.90
+  },
+  {
+  nome: "X-Salada",
+  ingredientes: ["pão de hambúrguer", "carne bovina", "queijo prato", "alface", "tomate", "cebola roxa"],
+  valor: 12.90
+  },
+  {
+  nome: "X-Frango",
+  ingredientes: ["pão de hambúrguer", "carne de frango", "ovo frito", "tomate", "milho verde", "ervilha"],
+  valor: 18.90
+  },
+  {
+  nome: "X-Tudo",
+  ingredientes: ["pão de hambúrguer", "carne bovina", "bacon picado", "ovo", "mussarela", "alface"],
+  valor: 14.90
+  },
+  {
+  nome: "X-Egg",
+  ingredientes: ["pão de hambúrguer", "carne bovina", "muçarela", "ovo", "maionese"],
+  valor: 12.90
+  },
+  {
+  nome: "X-Calabresa",
+  ingredientes: ["pão de hambúrguer", "carne bovina", "calabresa picada", "alface", "presunto", "maionese"],
+  valor: 18.90
+  },
+];
+
+console.table(lanches);
+
+
+// let soma = (n1, n2) => n1 + n2;
+// console.log(soma(2, 3));
+//         OU
+// function soma(n1, n2) {
+//   return n1 + n2;
+// }
+// console.log(soma(2, 3));
+//         OU
+// let somar = function(n1, n2) {
+//   return n1 + n2;
+// }
+// console.log(somar(2, 3));
+//___________________________________________________________________________
+ 
+//de caracteres dessa string.
+// function contarCaracteres(str) {
+//   return str.length;
+// }
+
+// console.log(contarCaracteres("Edilson"));
+//___________________________________________________________________________
+
+// function soma(a, b) {
+//   return a + b;
+// }
+
+// console.log(soma(5, 6));
+//___________________________________________________________________________
+
+// function saudacao(nome) {
+//   console.log(`Olá ${nome}!`);
+// }
+
+// saudacao("Edilson");
+//___________________________________________________________________________
+
+// function quadrado(numero) {
+//   return numero * numero;
+// }
+
+// console.log(quadrado(3));
+//___________________________________________________________________________
+
+// function parOuImpar(numero) {
+//   if(numero % 2 == 0) {
+//     return "par";
+//   } else {
+//     return "impar";
+//   }
+// }
+
+// console.log(parOuImpar(0));
+//___________________________________________________________________________
+
+// function inDivisible(num, divisor) {
+//   return num % divisor === 0;
+// }
+
+// console.log(inDivisible(10, 2));
+//___________________________________________________________________________
+
